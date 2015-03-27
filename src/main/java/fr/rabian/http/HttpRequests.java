7db4ce3b3d1742008646632f767c62e333950223
Adrien J-M. C. Rabian <adrien@rabian.fr>
2015-03-27 19:55:09 +0100

    Working requests for GET and POST methods.

---
+++ src/main/java/fr/rabian/http/HttpRequests.java
@@ -0,0 +1,64 @@
+package fr.rabian.http;
+
+/**
+ * Created by adrien on 27/03/15.
+ */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public abstract class HttpRequests {
+
+    public static int sendGet(String url, StringBuffer out) throws Exception {
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        int responseCode = con.getResponseCode();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) {
+            out.append(inputLine);
+        }
+        in.close();
+
+        return responseCode;
+    }
+
+    public static int sendPost(String url, StringBuffer out, String urlParameters) throws Exception {
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("POST");
+
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) {
+            out.append(inputLine);
+        }
+        in.close();
+
+        return responseCode;
+    }
+
+}
