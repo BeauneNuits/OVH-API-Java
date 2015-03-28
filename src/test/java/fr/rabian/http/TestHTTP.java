c7509b77f631ae921cdb94b054117354522387df
Adrien J-M. C. Rabian <adrien@rabian.fr>
2015-03-28 23:46:44 +0100

    Add some tests to get time.

--- src/test/java/fr/rabian/http/TestHTTP.java
+++ src/test/java/fr/rabian/http/TestHTTP.java
@@ -3,10 +3,12 @@ package fr.rabian.http;
 /**
  * Created by adrien on 27/03/15.
  */
+
 public class TestHTTP {
     public static void main(String[] args) throws Exception {
         StringBuffer out = new StringBuffer();
-        HttpRequests.sendPost("http://requestb.in/1aamhie1", out, "nom=Adrien&prenom=Rabian");
-        System.out.println(out.toString());
+        HttpRequests.sendGet("https://eu.api.ovh.com/1.0/auth/time", out);
+        System.out.println("OVH : " + out.toString());
+        System.out.println("System : " + (int) (System.currentTimeMillis() / 1000L));
     }
 }
