a064bfa4abc24ef8110cfbafaaf40813674749a7
Adrien J-M. C. Rabian <adrien@rabian.fr>
2015-04-09 11:45:48 +0200

    Modifying Customer to satisfy OVH names.

--- src/main/java/fr/rabian/ovhApi/Customer.java
+++ src/main/java/fr/rabian/ovhApi/Customer.java
@@ -7,30 +7,30 @@ import java.util.List;
  */
 public class Customer {
 
-    private final String cKey;
+    private final String consumerKey;
     private List<ScopeElement> scope;
     private String validationUrl;
     private String state;
 
 
 
-    protected Customer(String cKey, List<ScopeElement> scope, String validationUrl, String state) {
-        this.cKey = cKey;
+    protected Customer(String consumerKey, List<ScopeElement> scope, String validationUrl, String state) {
+        this.consumerKey = consumerKey;
         this.scope = scope;
         this.validationUrl = validationUrl;
         this.state = state;
     }
 
-    protected Customer(String cKey) {
-        this(cKey, null, null, null);
+    protected Customer(String consumerKey) {
+        this(consumerKey, null, null, null);
     }
 
     protected Customer() {
         this(null, null, null, null);
     }
 
-    public String getcKey() {
-        return cKey;
+    public String getConsumerKey() {
+        return consumerKey;
     }
 
     public List<ScopeElement> getScope() {
