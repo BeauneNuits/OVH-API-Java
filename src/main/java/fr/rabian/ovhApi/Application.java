fe267f306ae06d8441d3cdf4f2196b62b478171b
Adrien J-M. C. Rabian <adrien@rabian.fr>
2015-04-09 11:38:13 +0200

    Adding function getCustomer to ,Application.

--- src/main/java/fr/rabian/ovhApi/Application.java
+++ src/main/java/fr/rabian/ovhApi/Application.java
@@ -1,5 +1,13 @@
 package fr.rabian.ovhApi;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import fr.rabian.ovhApi.http.HttpRequests;
+import fr.rabian.ovhApi.requestBeans.RequestCredentials;
+import fr.rabian.ovhApi.requestBeans.RequestProperty;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Adrien on 31/03/2015.
  */
@@ -13,6 +21,30 @@ public class Application {
         this.name = name;
         this.pubKey = pubKey;
         this.secKey = secKey;
+    }
+
+    public Customer getCustomer(List<ScopeElement> scope, String redirectURL) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        List<RequestProperty> headers = new ArrayList<>();
+        headers.add(new RequestProperty("Content-type", "application/json"));
+        headers.add(new RequestProperty("X-Ovh-Application", this.pubKey));
+        StringBuffer out = new StringBuffer();
+
+        RequestCredentials rc = new RequestCredentials(scope, redirectURL);
+        String req = gson.toJson(rc);
+        int result = 0;
+        try {
+            result = HttpRequests.sendPost("https://eu.api.ovh.com/1.0/auth/credential", out, req, headers);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Customer c = null;
+        if (result == 200) {
+            c = gson.fromJson(out.toString(), Customer.class);
+            c.setScope(scope);
+        }
+        return c;
     }
 
 }
