ef337a39062f6438d1aee30022fc7f8292382130
Adrien J-M. C. Rabian <adrien@rabian.fr>
2015-04-10 20:22:51 +0200

    Javadoc Header.

--- src/main/java/fr/rabian/ovhApi/http/Header.java
+++ src/main/java/fr/rabian/ovhApi/http/Header.java
@@ -1,21 +1,45 @@
 package fr.rabian.ovhApi.http;
 
 /**
- * Created by Adrien on 09/04/2015.
+ * Represents an HTTP header for use in HTTP requests.
  */
 public class Header {
+
+    /**
+     * The header's title
+     */
     String key;
+
+    /**
+     * The header's value
+     */
     String value;
 
+    /**
+     * Creates a header.
+     *
+     * @param key Title
+     * @param value Value
+     */
     public Header(String key, String value) {
         this.key = key;
         this.value = value;
     }
 
+    /**
+     * Returns the title.
+     *
+     * @return Key
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Returns the value.
+     *
+     * @return Value
+     */
     public String getValue() {
         return value;
     }
