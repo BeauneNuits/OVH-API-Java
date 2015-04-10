0213c309ed108560a35185e74e22a8e23f58f99b
Adrien J-M. C. Rabian <adrien@rabian.fr>
2015-04-10 19:34:53 +0200

    Javadoc Application.

--- src/main/java/fr/rabian/ovhApi/beans/Application.java
+++ src/main/java/fr/rabian/ovhApi/beans/Application.java
@@ -1,28 +1,60 @@
 package fr.rabian.ovhApi.beans;
 
 /**
- * Created by Adrien on 31/03/2015.
+ * Represents an OVH application.
+ *
+ * @author Adrien Rabian
  */
 public class Application {
-
+    /**
+     * Application's name
+     */
     private final String name;
+    /**
+     * Application key (AK)
+     */
     private final String pubKey;
+    /**
+     * Application secret (AS)
+     */
     private final String secKey;
 
+    /**
+     * Creates an application.
+     *
+     * @param name Application's name
+     * @param pubKey Application key (AK)
+     * @param secKey Application secret (AS)
+     */
     public Application(String name, String pubKey, String secKey) {
         this.name = name;
         this.pubKey = pubKey;
         this.secKey = secKey;
     }
 
+    /**
+     * Returns the app's name.
+     *
+     * @return Application's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the app's public key.
+     *
+     * @return Application key (AK)
+     */
     public String getPubKey() {
         return pubKey;
     }
 
+    /**
+     * Returns the app's secret key.
+     *
+     * @return Application secret (AS)
+     */
     public String getSecKey() {
         return secKey;
     }
