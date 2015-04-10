76bdcbfeef41ad3af99064f96b55b3c1c714b284
Adrien J-M. C. Rabian <adrien@rabian.fr>
2015-04-10 17:24:16 +0200

    Correcting endpoints URLs.

--- src/main/java/fr/rabian/ovhApi/beans/Endpoint.java
+++ src/main/java/fr/rabian/ovhApi/beans/Endpoint.java
@@ -5,12 +5,12 @@ package fr.rabian.ovhApi.beans;
  */
 public class Endpoint {
 
-    public static final Endpoint ovh_eu = new Endpoint("OVH Europe", "https://eu.api.ovh.com/1.0/");
-    public static final Endpoint ovh_ca = new Endpoint("OVH North-America", "https://ca.api.ovh.com/1.0/");
+    public static final Endpoint ovh_eu = new Endpoint("OVH Europe", "https://eu.api.ovh.com/1.0");
+    public static final Endpoint ovh_ca = new Endpoint("OVH North-America", "https://ca.api.ovh.com/1.0");
     public static final Endpoint sys_eu = new Endpoint("So you Start Europe", "https://eu.api.soyoustart.com/1.0");
     public static final Endpoint sys_ca = new Endpoint("So you Start North America", "https://eu.api.soyoustart.com/1.0");
-    public static final Endpoint ks_eu = new Endpoint("Kimsufi Europe", "https://eu.api.kimsufi.com/1.0/");
-    public static final Endpoint ks_ca = new Endpoint("Kimsufi North America", "https://ca.api.kimsufi.com/1.0/");
+    public static final Endpoint ks_eu = new Endpoint("Kimsufi Europe", "https://eu.api.kimsufi.com/1.0");
+    public static final Endpoint ks_ca = new Endpoint("Kimsufi North America", "https://ca.api.kimsufi.com/1.0");
     public static final Endpoint ra_ca = new Endpoint("Runabove", "https://api.runabove.com/1.0");
 
     private String name;
