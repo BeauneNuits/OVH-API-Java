5d45a37f1338dffe9d728ca0920ca4588832575d
Adrien J-M. C. Rabian <adrien@rabian.fr>
2015-04-13 14:10:56 +0200

    Improving NOKResponseException : store the code.

--- src/main/java/fr/rabian/ovhApi/core/utils/NOKResponseException.java
+++ src/main/java/fr/rabian/ovhApi/core/utils/NOKResponseException.java
@@ -6,7 +6,15 @@ import java.io.IOException;
  * @author Adrien Rabian
  */
 public class NOKResponseException extends IOException {
+
+    private int responseCode;
+
     public NOKResponseException(int responseCode) {
         super("HTTP response code " + responseCode + " encountered.");
+        this.responseCode = responseCode;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
     }
 }
